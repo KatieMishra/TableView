1a0527221a5134ce41ee57f93eb1cf5c05625ac7
Katie Mishra <katiemishra@Katies-MacBook-Air.local>
2017-10-27 09:37:33 -0700

    added project

---
+++ ToDoListApp/ToDoListApp/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Katie Mishra on 10/27/17.
+//  Copyright © 2017 ATCS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    var toDoList = ["walk the dog", "make the bed", "buy groceries", "mani-pedi", "call mom", "walk the dog", "make the bed", "buy groceries", "mani-pedi", "call mom", "walk the dog", "make the bed", "buy groceries", "mani-pedi", "call mom"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuse")
+        let text = toDoList[indexPath.row]
+        cell!.textLabel?.text = text
+        return cell!
+    }
+
+}
+
